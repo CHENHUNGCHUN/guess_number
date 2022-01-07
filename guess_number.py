45f5a594e71d8dba1f2c8170b591fa155e1588a5
Marco Chen <kojunjunla@outlook.com>
2022-01-07 19:21:02 +0800

    update_ver1

--- guess_number.py
+++ guess_number.py
@@ -1,15 +1,17 @@
 import random
 
 r = random.randint(1,100)
-
+count = 0
 while True:
+    count+=1
     number =eval(input('請輸入一個'))
     if r == number :
-        print('答對')
+        print('猜了', count,'次答對')
         break
     else:
-        print('猜錯囉')
         if r> number:
-            print('往上猜')
+            print('猜錯囉，往上猜')
+            print('目前猜了第', count,'次')
         elif r<number:
-            print('往下猜')
+            print('猜錯囉，往下猜')
+            print('目前猜了第', count,'次')
